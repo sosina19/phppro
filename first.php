f7207528a894044295261ec04472d95bd9c19226
sosina19 <yeshidnbersosina@gmail.com>
2025-03-04 20:58:24 +0300

    Initial commit

---
+++ first.php
@@ -0,0 +1,23 @@
+<?php
+
+$x="45";
+$y="fgtr yt455";
+$e=array("cee","ggg'","jjj");
+var_dump($x);
+echo"<br>";
+echo strlen($y);
+echo"<br>";
+echo str_word_count($y);
+echo"<br>";
+echo strrev($y);
+echo"<br>";
+echo strpos($y,"4");
+echo"<br>";
+echo str_replace("yt","gggh","fgtr yt455");
+echo"<br>";
+var_dump(is_float($x));
+echo"<br>";
+var_dump($e);
+
+
+?>
